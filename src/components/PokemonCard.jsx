6021cd934c5b6ee2a1be02911a37041247254558
[Ryad] <[ryadzaatour@gmail.com]>
2024-05-17 09:18:45 +0200

    Pokemon list dynamique

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
-function PokemonCard()  {
-  return (
-    <figure>
-      <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur" 
-      />
-    <figcaption>
-        Bulbasaur
-    </figcaption>
-    </figure>
-  );
-};
+const PokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  const PokemonCard = () => {
+    
+    const pokemon = PokemonList[0];
+  
+    return (
+      <figure>
+        <figcaption>{pokemon.name}</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+      </figure>
+    );
+  };
+
 
 export default PokemonCard;
